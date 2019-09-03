980b48c42fbb62f426ae4a6597d99dba88dd5835
tyobigoro <n.n.6.suke@gmail.com>
2019-09-03 14:59:18 +0900

    Initial Commit

---
+++ tChangeViewVisiblity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tChangeViewVisiblity
+//
+//  Created by tyobigoro on 2019/09/03.
+//  Copyright © 2019 tyobigoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
